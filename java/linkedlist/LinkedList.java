fa5c99000e0872a7600720c53cd9c7b881baf93d
Pablo Molnar <pablo.molnar@hulu.com>
2018-09-25 10:33:08 -0700

    linkedlist

--- java/linkedlist/LinkedList.java
+++ java/linkedlist/LinkedList.java
@@ -1,9 +1,12 @@
 package linkedlist;
 
+import java.util.HashSet;
+
 public class LinkedList {
     class Node {
         int value;
-    
+        Node next;
+
         public Node(int value) {
             this.value = value;
         }
@@ -17,7 +20,7 @@ public class LinkedList {
 
     public LinkedList add(int value) {
         Node tail = this.head;
-        while(tail.next != null) {
+        while (tail.next != null) {
             tail = tail.next;
         }
 
@@ -29,16 +32,16 @@ public class LinkedList {
     public void remove(int value) {
         Node prev = null;
         Node n = this.head;
-        while(n != null && n.value != value) {
+        while (n != null && n.value != value) {
             prev = n;
             n = n.next;
         }
 
         // n is null then we didn't found the value
-        if(n == null) return;
+        if (n == null) return;
 
         // n is head
-        if(prev == null) {
+        if (prev == null) {
             this.head = this.head.next;
         } else {
             prev.next = n.next;
@@ -46,12 +49,17 @@ public class LinkedList {
     }
 
     public void unique() {
-        if(this.head == null) return;
+        if (this.head == null) return;
+
+        Node prev = null;
         Node n = this.head;
-        var values = new HashSet<Integer>(n.value);
-        while(n.next != null) {
-            if(values.contains(n.next.value)) {
-                n.next = n.next.next;
+        var values = new HashSet<Integer>();
+        while (n != null) {
+            if (values.contains(n.value)) {
+                prev.next = n.next;
+            } else {
+                prev = n;
+                values.add(n.value);
             }
 
             n = n.next;
@@ -60,17 +68,41 @@ public class LinkedList {
 
     public void print() {
         Node n = this.head;
-        while(n!= null) {
+        while (n != null) {
             System.out.format("%s -> ", n.value);
+            n = n.next;
         }
         System.out.println("DONE!");
     }
 
-    public static void main(String[] args)  { 
-        var list = new LinkedList(1).add(1).add(2).add(3).add(2).add(1);
-        list.unique();
+    public Integer findKthLast(int pos) {
+        Node n = this.head;
+        Node kth = this.head;
+        int i = 0;
+        while (n != null) {
+            if (++i > pos) {
+                kth = kth.next;
 
-        list.print();
-    } 
+            }
+
+            n = n.next;
+        }
+
+        if (i >= pos) {
+            return kth.value;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        var list = new LinkedList(1).add(1).add(2).add(3).add(3).add(1).add(2).add(1).add(2);
+
+        System.out.println(list.findKthLast(8));
+        System.out.println(list.findKthLast(50));
+
+        // list.unique();
+        // list.print();
+    }
 }
 
